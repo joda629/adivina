d1a81d47bbc957681bf907e8fbf672f7d5052fe9
joda <yorgy629@hotmail.com>
2020-04-21 08:35:08 -0500

    Listo para subir

---
+++ README.md
@@ -0,0 +1,3 @@
+# Juego de adivinar un número entre 1 y 20
+
+## Tiene solo 6 vidas para lograrlo, de lo contrario se iniciará el juego de nuevo, el juego le irá indicando si el número que desea adivinar es mas grande o pequeño, en comparación con el número que ingresó
---
+++ otro.js
@@ -0,0 +1,74 @@
+let submit= document.getElementById("submit")
+let random=Math.ceil(Math.random() *20 )
+let info=document.getElementById("info")
+let num = document.getElementById("number")
+let int=6;
+let show=document.getElementById("show")
+let img=document.querySelector("img")
+let form=document.getElementById("form")
+
+
+submit.addEventListener("click", function(e)
+{
+    if(num.value == "")
+    {
+        alert("Debes ingresar un número");
+        window.location.reload()
+    }
+    else 
+    {
+        if(int > 0)
+        {
+            if(num.value < random)
+            {
+                info.innerHTML="El número es mas grande";
+                int--;
+                show.removeChild(show.children[0])
+            }
+            else if(num.value > random)
+            {
+                
+                info.innerHTML="El número es mas pequeño";
+                int--;
+
+                show.removeChild(show.children[0])
+            }
+            else if(num.value == random)
+            {
+                if(int == 1)
+                {
+                    alert("Felicidades adivinaste el número " + random + " y te quedó " + int + " vida");
+                    window.location.reload()
+                    num.value=""
+                }
+                else 
+                {
+                    alert("Felicidades adivinaste el número " + random + " y te quedaron " + int + " vidas");
+                    window.location.reload()
+                    num.value=""
+                }
+            }
+        }
+        else 
+        {
+            alert("Lo sentimos no tienes mas vidas")
+            window.location.reload()
+            num.value=""
+        }
+
+
+        
+    }
+    
+    
+})
+
+
+num.addEventListener("click", () =>
+{
+        let label=document.getElementById("label")
+
+        label.classList.add("up")
+})
+
+//document.write(random)
