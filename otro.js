bcc16bfc21d805ca38fdd32442ecf97fdd014c2d
joda <yorgy629@hotmail.com>
2020-04-21 09:52:52 -0500

    ok

--- otro.js
+++ otro.js
@@ -38,14 +38,12 @@ submit.addEventListener("click", function(e)
                 if(int == 1)
                 {
                     alert("Felicidades adivinaste el número " + random + " y te quedó " + int + " vida");
-                    location.href="https://joda629.github.io/adivina"
                     window.location.reload()
                    num.value=""
                 }
                 else 
                 {
                     alert("Felicidades adivinaste el número " + random + " y te quedaron " + int + " vidas");
-                    location.href="https://joda629.github.io/adivina"
                     window.location.reload()
                    num.value=""
                 }
@@ -54,7 +52,6 @@ submit.addEventListener("click", function(e)
         else 
         {
             alert("Lo sentimos no tienes mas vidas")
-            location.href="https://joda629.github.io/adivina"
             window.location.reload()
             num.value=""
         }
